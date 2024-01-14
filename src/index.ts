d0364f5badc434b3fcde28e51216cd401c35c987
Tom Gao <tom@tomgao.cc>
2024-01-14 11:05:42 +0800

    feat: add configuration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
-import { StatusBarAlignment, window } from 'vscode';
+import { StatusBarAlignment, window, workspace } from 'vscode';
+
+const CONFIG_POSITION = 'tomjs.reload.position';
+const CONFIG_TEXT = 'tomjs.reload.text';
+const CONFIG_TOOLTIP = 'tomjs.reload.tooltip';
+
+function getConfig(key: string): string | undefined {
+  return workspace.getConfiguration().get(key);
+}
 
 export function activate() {
-  const statusBar = window.createStatusBarItem(StatusBarAlignment.Right, 0);
+  const position =
+    getConfig(CONFIG_POSITION) === 'left' ? StatusBarAlignment.Left : StatusBarAlignment.Right;
+  const statusBar = window.createStatusBarItem(position, 0);
 
-  statusBar.text = 'Reload';
-  statusBar.command = 'workbench.action.reloadWindow';
-  statusBar.tooltip = 'Reload window';
+  statusBar.text = getConfig(CONFIG_TEXT) || 'Reload';
+  statusBar.tooltip = getConfig(CONFIG_TOOLTIP) || 'Reload window';
+  statusBar.command = `workbench.action.reloadWindow`;
 
   statusBar.show();
 }
